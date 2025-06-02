a12b0f4d267684df19db78bb261eb7fe2f31b31b
Muhammadali <sadullohmuhammadali@gmail.com>
2025-06-03 01:19:27 +0500

    Transport classification project

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+
+import plotly.express as px
+import platform
+
+plt=platform.system()
+if plt=='Linux':
+    pathlib.WindowsPath=pathlib.PosixPath
+else:
+    temp=pathlib.PosixPath
+    pathlib.PosixPath=pathlib.WindowsPath
+
+
+st.title('Klassifikatsiya qilish')
+
+file=st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'jpg'])
+if file:
+    st.image(file)
+    # PIL convert
+    img=PILImage.create(file)
+    # model
+    model=load_learner('transport_model5.pkl')
+
+    # prediction
+    pred,pred_id,probs=model.predict(img)
+    st.success(f'Bashorat:{pred}')
+    st.info(f'Ehtimollik:{probs[pred_id]:.3%}')
+    fig=px.bar(x=model.dls.vocab,y=probs*100)
+    st.plotly_chart(fig)
